9529a435bb486e1f253ee3ead0a1decf25a18390
Stefan Latinović <stefan.latinovic@outlook.com>
2025-09-07 10:12:38 +0200

    fix: correct typo in contract name (Challlenge01 -> Challenge01)

--- src/Challenge01.sol
+++ src/Challenge01.sol
@@ -9,7 +9,7 @@
 
 pragma solidity ^0.8.20;
 
-contract Challlenge01 {
+contract Challenge01 {
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
